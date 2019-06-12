ae74b59922c422be0ed399ddd427ea72d068bd03
vitorino87 <brother_87_22@hotmail.com>
2019-06-12 20:18:37 -0300

    adicionado a atualizacao da pagina e esta funcionando

--- javascript2.js
+++ javascript2.js
@@ -1,3 +1,8 @@
+var hora = ['09','13','14','18'];
+var minuto = ['00'];
+
+var c1 = setInterval(atualizarP,3000);
+
 function myFunction(){
 	chrome.tabs.executeScript(
 		{
@@ -30,5 +35,37 @@ function myFunction(){
 };
 
 document.addEventListener('DOMContentLoaded',function(){
-	document.querySelector('button').addEventListener('click', myFunction);
+	document.querySelector('button').addEventListener('click', myTimer);	
 });
+
+function myTimer(){
+	/*setInterval(addTime(),60000);	/*função para */
+	/*var c1 = setInterval(atualizarP,3000);	*/
+}
+
+function addTime(){
+	var i;
+	var d = new Date();
+	for(i=0;i<hora.length;i++){
+		if(d.getHours()==hora[i]){
+			
+		}
+	}	
+}
+
+function atualizarPag(){
+	document.getElementById("elgen-4").click();		
+}
+function atualizarPag2(){
+	document.getElementById("elgen-8").click();
+} 
+
+function atualizarP(){
+	chrome.tabs.executeScript(
+		{				
+			code: 'document.getElementById("idFormDadosEntradaSaida").submit()' /*serve para submeter o form idFormDadosEntradaSaida*/
+		}
+	);
+	chrome.tabs.reload();
+	chrome.runtime.reload();	
+}
